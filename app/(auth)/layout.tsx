2add265c16c9a644a09ad59062d4cb6ee0575711
meshackrono10 <meshackrono05@gmail.com>
2024-04-22 15:05:26 +0300

    some random commits

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,6 +5,7 @@ const AuthLayout = ({
 }) => {
   return ( 
     <div className="h-full flex items-center justify-center">
+      <h2>Welcome to SkillBled where you get you courses of your choice with favorable prices </h2>
       {children}
     </div>
    );
